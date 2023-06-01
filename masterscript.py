719ae397753feae605cd14e762015dc0ebf658aa
lisatran251 <106535956+lisatran251@users.noreply.github.com>
2023-06-01 04:07:38 -0400

    Update masterscript.py

--- masterscript.py
+++ masterscript.py
@@ -1,35 +1,39 @@
 import pandas as pd
 import csv
 import re
+from itertools import product
 
 # Read the primer CSV file into a DataFrame
 df = pd.read_csv('DARTE-QM_primer_design.csv')
 
-# Create new df for F_seq and R_seq
-prim_list = df[['F_truseq','R_truseq']]
+# Create lists for F_seq and R_seq
+F_primers = df['F_truseq'].tolist()
+R_primers = df['R_truseq'].tolist()
 
 # Create empty df for storing the results
 result_df = pd.DataFrame(columns=['F_truseq','R_truseq','Product'])
 
 # Define the sequence
-with open('filtered_contigs_ex.fastq','r') as file:
-	sequence = file.read()# replace with your actual sequence or read from file
+with open('ex.fastq','r') as file:
+    sequence = file.read() # replace with your actual sequence or read from file
 
-# Loop through each row in the primer df
-for _, row in prim_list.iterrows():
-    F_primer = row['F_truseq']
-    R_primer = row['R_truseq']
-    
-    pattern = f'{F_primer}(.*?){R_primer}'
-    # Search the sequence with the pattern
-    match = re.search(pattern, sequence)
-
-    if match:
-        # If found, append the result to the result df
-        result_df = result_df.append({
-            'F_truseq': F_primer,
-            'R_truseq': R_primer,
-            'Product': match.group(0)
-        }, ignore_index=True)    
+# Loop through each forward and reverse primer
+for F_primer in F_primers:
+    for R_primer in R_primers:
+        pattern = f'({F_primer})(.*?)({R_primer})'
+        # Search the sequence with the pattern
+        matches = re.findall(pattern, sequence)
+        
+        for match in matches:
+            # If found, append the result to the result df
+            result_df = result_df.append({
+                'F_truseq': match[0],
+                'R_truseq': match[2],
+                'Product': ''.join(match)
+            }, ignore_index=True)
 
 print(result_df)
+
+
+# Test 1: find sequences start with forward primer and end with reverse primer (PASS)
+# Test 2: find sequences start with any forward primer and end with any reverse primer (PASS)
