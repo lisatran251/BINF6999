efd00d609789c85edc490524543b0c25ee54ca4c
lisatran251 <106535956+lisatran251@users.noreply.github.com>
2023-06-02 01:16:19 -0400

    Update masterscript.py

--- masterscript.py
+++ masterscript.py
@@ -3,12 +3,15 @@ import csv
 import re
 from itertools import product
 
+# Test 1: find sequences start with forward primer and end with reverse primer (PASS)
+# Test 2: find sequences start with any forward primer and end with any reverse primer (PASS)
+
 # Read the primer CSV file into a DataFrame
 df = pd.read_csv('DARTE-QM_primer_design.csv')
 
 # Create lists for F_seq and R_seq
-F_primers = df['F_truseq'].tolist()
-R_primers = df['R_truseq'].tolist()
+F_primers = df['F_truseq'].unique().tolist()
+R_primers = df['R_truseq'].unique().tolist()
 
 # Create empty df for storing the results
 result_df = pd.DataFrame(columns=['F_truseq','R_truseq','Product'])
@@ -33,7 +36,3 @@ for F_primer in F_primers:
             }, ignore_index=True)
 
 print(result_df)
-
-
-# Test 1: find sequences start with forward primer and end with reverse primer (PASS)
-# Test 2: find sequences start with any forward primer and end with any reverse primer (PASS)
