0ec5aced0488997f8493d161f4fa8fdea5c29f4c
lisatran251 <106535956+lisatran251@users.noreply.github.com>
2023-06-09 20:18:40 -0400

    Update masterscript.py

--- masterscript.py
+++ masterscript.py
@@ -1,4 +1,5 @@
 # python -m venv ~/my_venv
+
 # source ~/my_venv/bin/activate
 # pip install pandas numpy biopython argparse
 
@@ -13,13 +14,17 @@
 # Test 6 (del): count occurence of ea. target gene (PASS)
 # Test 7: Overlaps 
 
+# brew install brewsci/bio/abricate
+# abricate --check
+# abricate --list
+
 #How to run: python3 sample.py ex.fastq
 
 import pandas as pd
-import re
 from Bio.Seq import Seq
 from Bio import SeqIO
 import argparse
+import csv
 
 # Create the parser
 parser = argparse.ArgumentParser(description="Parse input and output file names")
@@ -29,16 +34,17 @@ parser.add_argument('input_file', type=str, help='Input file name')
 args = parser.parse_args()
 input_file = args.input_file
 
-# Read the primer CSV file into a DataFrame
+# Read the primer csv file into a df
 df = pd.read_csv('DARTE-QM_primer_design.csv')
-# Add a new column to the DataFrame with the reverse complement of the product
+
+# Add a new column to the df with the reverse complement of the product
 df['product_reverse_complement'] = df['product'].apply(lambda x: str(Seq(x).reverse_complement()))
 
 # Create dictionaries for primers in regular and reverse complement primers
 primer_dict = {}
 reverse_primer_dict = {}
 
-
+# Loop through df to find product, length, target gene and gene locus 
 for _, row in df.iterrows():
     key = (row['product'], int(row['Product_length']))
     value = {'target_gene': row['target_gene'], 'target_locus': row['target_locus']}
@@ -49,98 +55,52 @@ for _, row in df.iterrows():
     reverse_primer_dict[rev_key] = value
 
 # Create lists for F_seq and R_seq
-F_primers = df['F_truseq'].unique().tolist()
-R_primers = df['R_truseq'].unique().tolist()
+F_primers = set(df['F_truseq'].unique().tolist())
+R_primers = set(df['R_truseq'].unique().tolist())
 
 # Create reverse complement for each primer 
-F_reverse_complement = [str(Seq(primer).reverse_complement()) for primer in F_primers]
-R_reverse_complement = [str(Seq(primer).reverse_complement()) for primer in R_primers]
+F_reverse_complement = {str(Seq(primer).reverse_complement()) for primer in F_primers}
+R_reverse_complement = {str(Seq(primer).reverse_complement()) for primer in R_primers}
 
-# Precompile regular expressions
-patterns = [(re.compile(f'({F_primer})(.*?)({R_primer})'), F_primer, R_primer) 
-            for F_primer in F_primers for R_primer in R_primers]
-
-rev_patterns = [(re.compile(f'({R_rev_comp})(.*?)({F_rev_comp})'), R_rev_comp, F_rev_comp) 
-                for F_rev_comp in F_reverse_complement for R_rev_comp in R_reverse_complement]
-
-# Store results in lists first
-results = []
-rev_results = []
+# Merge 4 lists of primers 
+all_primers = F_primers | R_primers | F_reverse_complement | R_reverse_complement
 
 # Read the sequence
 with open(input_file,'r') as handle:
     records = SeqIO.parse(handle, 'fasta')
+    with open('out_test.csv', 'w', newline='') as result_file:
+        csv_writer = csv.writer(result_file)
+        csv_writer.writerow(['Product', 'Start position', 'End position', 'Length', 'Start Primer', 'End Primer', 'From reverse complement', 'Target Gene'])
 
-    for record in records:
-        sequence = str(record.seq)
-        
-        # Loop through each pattern
-        for pattern, F_primer, R_primer in patterns:
-            matches = pattern.finditer(sequence)
-            for match in matches:
-                # Append the results to the list
-                results.append({
-                    'F_primer': F_primer,
-                    'R_primer': R_primer,
-                    'Product': match.group(),
-                    'Start': match.start(),
-                    'End': match.end(),
-                    'Length': match.end() - match.start()
-                })
+        # Iterate over each sequence record in the input file
+        for record in records:
+            # Convert the sequence record to a string
+            sequence = str(record.seq)
 
-        for rev_pattern, F_rev_comp, R_rev_comp in rev_patterns:
-            rev_matches = rev_pattern.finditer(sequence)
-            for match in rev_matches:
-                rev_results.append({
-                    'F_reverse_complement': F_rev_comp,
-                    'R_reverse_complement': R_rev_comp,
-                    'Product': match.group(),
-                    'Start': match.start(),
-                    'End': match.end(),
-                    'Length': match.end() - match.start()
-                })
+            # Iterate over each combination of forward and reverse primers
+            for f_primer in F_primers:
+                for r_primer in R_primers:
+                    # Find the positions of the primers in the sequence
+                    f_pos = sequence.find(f_primer)
+                    r_pos = sequence.find(r_primer)
 
-# Convert results to DataFrame after the loop
-result_df = pd.DataFrame(results)
-result_reverse_df = pd.DataFrame(rev_results)
+                    # If the forward primer comes before the reverse primer and both primers are found
+                    if f_pos < r_pos and f_pos != -1 and r_pos != -1:
+                        # Extract the product sequence
+                        product = sequence[f_pos:r_pos+len(r_primer)]
+                        # Get the target gene name from the primer dictionary, or 'N/A' if not found
+                        target_gene = primer_dict.get((product, len(product)), {}).get('target_gene', 'N/A')
+                        # Write the information to the csv file
+                        csv_writer.writerow([product, f_pos, r_pos + len(r_primer), len(product), f_primer, r_primer, 'No', target_gene])
 
+            # Repeat the process with reverse complement primers
+            for f_rev_comp in F_reverse_complement:
+                for r_rev_comp in R_reverse_complement:
+                    r_pos = sequence.find(r_rev_comp)
+                    f_pos = sequence.find(f_rev_comp)
 
-# result_df.to_csv('result.csv', sep='\t', index=False)
-# result_reverse_df.to_csv('result_reverse.csv', sep='\t', index=False)
+                    if r_pos < f_pos and r_pos != -1 and f_pos != -1:
+                        product = sequence[r_pos:f_pos+len(f_rev_comp)]
+                        target_gene = reverse_primer_dict.get((product, len(product)), {}).get('target_gene', 'N/A')
+                        csv_writer.writerow([product, r_pos, f_pos + len(f_rev_comp), len(product), r_rev_comp, f_rev_comp, 'Yes', target_gene])
 
-# Iterate through result_df and result_reverse_df to check the matching keys in primer_dict
-result_df['Target gene'] = 'N/A'
-result_reverse_df['Target gene'] = 'N/A'
-
-for index, row in result_df.iterrows():
-    key = (row['Product'], row['Length'])
-    if key in primer_dict:
-        result_df.loc[index, 'Target gene'] = primer_dict[key]['target_gene']
-
-for index, row in result_reverse_df.iterrows():
-    key = (row['Product'], row['Length'])
-    if key in reverse_primer_dict:
-        result_reverse_df.loc[index, 'Target gene'] = reverse_primer_dict[key]['target_gene']
-
-print(result_df)
-print(result_reverse_df)
-# result_df.to_csv('trial1.csv', sep='\t', index=False)
-# result_reverse_df.to_csv('trial1_plus.csv', sep='\t', index=False)
-
-# # Count the occurrences of each gene in result_df
-# result_df_counts = result_df.groupby('Target gene').size().reset_index(name='Count')
-
-# # Count the occurrences of each gene in result_reverse_df
-# result_reverse_df_counts = result_reverse_df.groupby('Target gene').size().reset_index(name='Count')
-
-# # Concatenate the results
-# total_counts = pd.concat([result_df_counts, result_reverse_df_counts])
-
-# # Group by the gene name and sum the counts
-# total_counts = total_counts.groupby('Target gene')['Count'].sum().reset_index()
-
-# # # Print the new DataFrame
-# # print(total_counts)
-
-# # Save the new DataFrame to a CSV file
-# total_counts.to_csv('gene_counts.csv', sep='\t', index=False)
