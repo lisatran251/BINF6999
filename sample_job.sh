810fd51a3cc6022e6b5645b68a0faa0006cf89e8
lisatran251 <106535956+lisatran251@users.noreply.github.com>
2023-06-11 16:26:14 -0400

    Update sample_job.sh

--- sample_job.sh
+++ sample_job.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --account=def-nricker
-#SBATCH --time=0-04:00
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --mem=800M
-#SBATCH --job-name=trial3_job
-#SBATCH --output=trial3_job.out
+#SBATCH --time=0-168:00
+#SBATCH --nodes=5
+#SBATCH --ntasks-per-node=5
+#SBATCH --mem=8G
+#SBATCH --job-name=fulljob3
+#SBATCH --output=fulljob3.out
 
 # Run the script 
 python3 -m venv ~/my_venv
 source ~/my_venv/bin/activate
-pip install pandas numpy biopython 
-python3 sample.py contigs_short.fasta
+pip install pandas numpy biopython argparse
+python3 updatedscript.py contigs_ex.fasta
