5ab8e2f1aa1ba6529903fd278cfdf09346f244d9
lisatran251 <106535956+lisatran251@users.noreply.github.com>
2023-06-15 13:07:39 -0400

    Update fasta_splitter.sh

--- fasta_splitter.sh
+++ fasta_splitter.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --account=def-nricker
-#SBATCH --time=0-168:00
-#SBATCH --nodes=3
+#SBATCH --time=0-24:00
+#SBATCH --nodes=5
 #SBATCH --ntasks-per-node=5
-#SBATCH --mem=6G
-#SBATCH --job-name=trial1
-#SBATCH --output=trial1.out
+#SBATCH --mem=3G
+#SBATCH --job-name=trial4
+#SBATCH --output=trial4.out
 
 # Input fasta file
 fasta_file="contigs_ex.fasta"
@@ -14,7 +14,7 @@ fasta_file="contigs_ex.fasta"
 prefix="chunk_"
 
 # Number of sequences per file
-seqs_per_file=10000
+seqs_per_file=5000
 
 # Split the fasta file into chunks and submit a job for each chunk
-awk -v size="$seqs_per_file" -v pre="$prefix" 'BEGIN {n_seq=0; n_file=1; file=sprintf("%s%02d.fasta", pre, n_file);} /^>/ {if(n_seq%size==0 && n_seq>0){close(file); print n_file-1, file >> "info.txt"; system("sbatch script1.sh " file); file=sprintf("%s%02d.fasta", pre, ++n_file);} n_seq++;} {print >> file; } END {print n_file, file >> "info.txt"; system("sbatch script1.sh " file);}' $fasta_file
+awk -v size="$seqs_per_file" -v pre="$prefix" 'BEGIN {n_seq=0; n_file=1; file=sprintf("%s%02d.fasta", pre, n_file);} /^>/ {if(n_seq%size==0 && n_seq>0){close(file); print n_file-1, file >> "info.txt"; system("sbatch script3.sh " file); file=sprintf("%s%02d.fasta", pre, ++n_file);} n_seq++;} {print >> file; } END {print n_file, file >> "info.txt"; system("sbatch script3.sh " file);}' $fasta_file
